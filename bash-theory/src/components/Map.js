c0d41b148424114a334db77d6090d8787ae12108
Adam Szyluk <aszyluk@stevens.edu>
2021-11-22 23:51:16 -0500

    Added marker generation from list of objects
    Added auto-generation of markers on map from a list of objects with a location in them. This data be gathered later from a MongoDB query. For now, there is on click behavior and on double click behavior with little functionality, but this can be tweaked to get more information later.

--- bash-theory/src/components/Map.js
+++ bash-theory/src/components/Map.js
@@ -1,6 +1,8 @@
 // Code mostly obtained from react-google-maps documentation.
-import React from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import React, { useState, useEffect } from "react";
+import "../App.css";
+// import itemData from "../../../data/items"; // This will be for when the data functions get implemented.
 
 const containerStyle = {
   width: "400px",
@@ -14,14 +16,59 @@ const center = {
 };
 
 function Map() {
-  return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
-      </GoogleMap>
-    </LoadScript>
-  );
+  // Set states
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [locData, setLocData] = useState(undefined);
+
+  let renderData = () => {
+    //TODO: Implement
+    console.log("rendering");
+  };
+
+  let travel = () => {
+    //TODO: Implement
+    console.log("travelling");
+  };
+
+  useEffect(() => {
+    // On Load
+    let fetchData = async () => {
+      try {
+        let data = [{ location: { lat: 40.74237, lng: -74.02905 } }]; // = await itemData.getMarkerData();
+        setLocData(data);
+        setLoading(false);
+        setError(false);
+      } catch (e) {
+        setLocData(e);
+        setError(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  } else if (error) {
+    return <h2>Error: {locData}</h2>;
+  } else {
+    return (
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={15}>
+          {locData.map((data) => {
+            return (
+              <Marker
+                position={data.location}
+                onClick={() => renderData()}
+                onDblClick={() => travel()}
+              ></Marker>
+            );
+          })}
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    );
+  }
 }
 
 export default React.memo(Map);
