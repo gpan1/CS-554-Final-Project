269ba3bf5325c7b8b1f0b489225740eb07a92472
rtrshm <ashum@stevens.edu>
2021-11-23 02:27:09 -0500

    updated data testing module

--- bash-theory/src/data/test.js
+++ bash-theory/src/data/test.js
@@ -1,22 +1,36 @@
 const posts = require('./posts')
+const locations = require('./locations')
 
 const test = async () => {
+
+    const location = {
+        lat: 69,
+        lng: 69
+    }
+
     const post = {
         posterName: 'archibald',
         title: 'post title',
         content: 'post content',
         date: new Date(),
-        location: {
-            lat: 69,
-            lng: 69
-        }
+        location
     };
 
     try {
         const newPost = await posts.create(post);
-        console.log(newPost);
+        console.log("Created post: \n" + newPost);
     } catch (e) {
         console.log(e);
+    }
+
+    try {
+        const allPosts = await posts.getAll();
+        console.log("All posts: \n" + allPosts);
+    }
+
+    try {
+        const allLocations = await locations.getAll();
+        console.log("All locations: \n" + allLocations);
     }
 }
 
