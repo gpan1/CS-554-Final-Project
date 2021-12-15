fd5dd6e37c758df9aa06321dfe320db71ee2e0f5
Gavin Pan <55461412+gpan1@users.noreply.github.com>
2021-12-15 00:11:39 -0500

    Listings and Reviews Lists Basic Divs
    Listings and Reviews Lists Basic Divs and Dummy Data inserted
    Set up for cards later
    CSS inserted

--- bash-theory/src/components/Landing.js
+++ bash-theory/src/components/Landing.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import Header from './Header';
 import Map from './Map';
-
+import noImage from "../noImage.png";
 function Landing() {
     return (
         <>
@@ -11,6 +11,32 @@ function Landing() {
             <Row className='landingRow'>
                 <Col className='medColumn'>
                     <h1 className='side'>Popular Picks</h1>
+                    <div className='box1'>
+                        <div className='box2'>
+                            <img src={noImage} alt="Test No AvailableImage" className='box2img'></img>
+                        </div>
+                        <div className='box3'>
+                            <p>Patrick Hill. The man, the myth, the legend.</p>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div className='box1'>
+                        <div className='box2'>
+                            <img src="https://mutekiramen.com/images/overview-img.jpg" className='box2img'></img>
+                        </div>
+                        <div className='box3'>
+                            <p>Opened in 20XX, Muteki Ramen is a restaurant specializing in Japanese cuisine.</p>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div className='box1'>
+                        <div className='box2'>
+                            <img src={noImage} alt="Test No AvailableImage" className='box2img'></img>
+                        </div>
+                        <div className='box3'>
+                            <p>Opened in 20XX, Muteki Ramen is a restaurant specializing in Japanese cuisine.</p>
+                        </div>
+                    </div>
                 </Col>
                 <Col xs={5} className="lgColumn">
                     <div className='navButtons'>
@@ -35,6 +61,26 @@ function Landing() {
                 </Col>
                 <Col className='medColumn'>
                     <h1 className='side'>Recent Reviews</h1>
+                    <div className="box1">
+                        <p>Reviewer Name: John Booba</p>
+                        <p>Review Item: Adam Szyluk</p>
+                        <p>Rating: 0</p>
+                        <p>Review: Adam Stinky!</p>
+                    </div>
+                    <br></br>
+                    <div className="box1">
+                        <p>Reviewer Name: John Smith</p>
+                        <p>Review Item: Adam Szyluk</p>
+                        <p>Rating: 0</p>
+                        <p>Review: Adam Smelly!</p>
+                    </div>
+                    <br></br>
+                    <div className="box1">
+                        <p>Reviewer Name: Adam Szyluk</p>
+                        <p>Review Item: Adam Szyluk</p>
+                        <p>Rating: 5</p>
+                        <p>Review: I love his manly musk</p>
+                    </div>
                 </Col>
             </Row>
         </Container>
