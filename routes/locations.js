c038a508a1ee52c26f931f4020623a08cbd11add
Adam Szyluk <aszyluk@stevens.edu>
2021-12-19 18:28:25 -0500

    Added route for getting locations by tags
    See above

--- routes/locations.js
+++ routes/locations.js
@@ -1,73 +1,95 @@
-const axios = require('axios');
-const express = require('express');
+const axios = require("axios");
+const express = require("express");
 const router = express.Router();
-const data = require('../data/locations');
+const data = require("../data/locations");
 
-router.get('/all', async (req, res) => {
+router.get("/all", async (req, res) => {
   try {
     let all = await data.getAll();
     return res.json(all);
   } catch (e) {
-    return res.status.json({ error: e });
+    return res
+      .status(e instanceof TypeError ? 400 : e instanceof Error ? 404 : 500)
+      .json({ error: e });
   }
 });
 
-router.post('/add', async (req, res) => {
+router.post("/add", async (req, res) => {
   try {
     let all = await data.create(req.body);
     return res.json(all);
   } catch (e) {
     console.log(e);
-    return res.status(
-      e instanceof TypeError ? 400 : 
-      e instanceof Error ? 404 : 500)
+    return res
+      .status(e instanceof TypeError ? 400 : e instanceof Error ? 404 : 500)
       .json({ error: e });
   }
 });
 
-router.get('/addPost', async (req, res) => {
+router.get("/addPost", async (req, res) => {
   try {
     let all = await data.addPost(req.body.locationId, req.body.postId);
     return res.json(all);
   } catch (e) {
-    return res.status.json({ error: e });
+    return res
+      .status(e instanceof TypeError ? 400 : e instanceof Error ? 404 : 500)
+      .json({ error: e });
   }
 });
 
-router.get('/byId/:id', async (req, res) => {
+router.get("/byId/:id", async (req, res) => {
   try {
     console.log(req.params.id);
     let all = await data.getLocById(req.params.id);
     return res.json(all);
   } catch (e) {
-    return res.status.json({ error: e });
+    return res
+      .status(e instanceof TypeError ? 400 : e instanceof Error ? 404 : 500)
+      .json({ error: e });
   }
 });
 
-router.patch('/update/:id', async (req, res) => {
+router.get("/byTags", async (req, res) => {
+  try {
+    let all = await data.getLocsByTags(req.body);
+    return res.json(all);
+  } catch (e) {
+    return res
+      .status(e instanceof TypeError ? 400 : e instanceof Error ? 404 : 500)
+      .json({ error: e });
+  }
+});
+
+router.patch("/update/:id", async (req, res) => {
   try {
     let all = await data.update(req.params.id, req.body);
     return res.json(all);
   } catch (e) {
-    return res.status.json({ error: e });
+    return res
+      .status(e instanceof TypeError ? 400 : e instanceof Error ? 404 : 500)
+      .json({ error: e });
   }
 });
 
-router.post('/remove/:id', async (req, res) => {
+router.post("/remove/:id", async (req, res) => {
   try {
     let all = data.remove(req.params.id);
     return res.json(all);
   } catch (e) {
-    return res.status.json({ error: e });
+    return res
+      .status(e instanceof TypeError ? 400 : e instanceof Error ? 404 : 500)
+      .json({ error: e });
   }
 });
 
-router.post('/search', async (req, res) => {
+router.post("/search", async (req, res) => {
   try {
     let all = await data.locSearch(req.body);
     return res.json(all);
   } catch (e) {
-    return res.status.json({ error: e });
+    return res
+      .status(e instanceof TypeError ? 400 : e instanceof Error ? 404 : 500)
+      .json({ error: e });
   }
 });
 
