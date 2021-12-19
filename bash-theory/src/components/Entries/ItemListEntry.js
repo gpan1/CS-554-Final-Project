4ce251a3408c51ecfc5ce923abf17e9e685af7fa
rtrshm <ashum@stevens.edu>
2021-12-19 18:52:57 -0500

    barebones version of ItemListEntry

--- bash-theory/src/components/Entries/ItemListEntry.js
+++ bash-theory/src/components/Entries/ItemListEntry.js
@@ -1,9 +1,23 @@
 import React from "react";
 
-function ItemListEntry(props){
+/**
+ * Meant for any list of items
+ * 
+ * Expected props: {name, description, avgRating, imgUrl (optional), address}
+ * @param {object} props 
+ * @returns 
+ */
+function ItemListEntry(props) {
     return (
         <div className="listEntry">
-            
+          <img src={props.imgUrl} alt={`image of ${props.name}`} />
+          <div class="listEntry-title-desc">
+            <h2> {props.name}</h2>
+            <p> {props.description} </p>
+          </div>
+          <p class="listEntry-avgRating">
+            {props.avgRating}
+          </p>
         </div>
     )
 }
