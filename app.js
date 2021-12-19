ab004af21947be047a075bcbffbdffb8060bbf34
rtrshm <ashum@stevens.edu>
2021-12-19 16:24:27 -0500

    fixed jest dependency; added cors support in express server

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 // const static = express.static(__dirname + '/public');
+
+app.use(cors());
 
 const configRoutes = require('./routes');
 
