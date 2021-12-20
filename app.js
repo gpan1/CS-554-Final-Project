bae24c57174d81150a023566fb020a7da7f64157
Jerry Chen <jchen1766@gmail.com>
2021-12-20 17:35:10 -0500

    'deploying'

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 // const static = express.static(__dirname + '/public');
@@ -12,11 +13,11 @@ const configRoutes = require('./routes');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(express.static(path.join(__dirname, 'bash-theory/build')));
+app.use(express.static(path.join(__dirname, 'bash-theory/build')));
 
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'bash-theory/build', 'index.html'));
-// });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'bash-theory/build', 'index.html'));
+});
 
 configRoutes(app);
 
