074a41239b224c8f5e4582e856479653e5866b49
Gavin Pan <55461412+gpan1@users.noreply.github.com>
2021-12-20 05:37:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const app = require('./app')
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:4000');
 });
+
+//localhost
+// app.listen(4000, () => {
+//   console.log("We've now got a server!");
+//   console.log('Your routes will be running on http://localhost:4000');
+// });
