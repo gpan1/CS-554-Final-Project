c0d31109dea1ed10c4fb69c182d7a7ac27f9e7a7
Gavin Pan <55461412+gpan1@users.noreply.github.com>
2021-12-20 04:35:05 -0500

    Heroku Port Changes Pls work

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./app')
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
   console.log("We've now got a server!");
   console.log('Your routes will be running on http://localhost:4000');
 });
